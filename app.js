163c239d2b5652a6d7766b7f3ffeffedc2bada37
HimanshuSingNegi <hnegi2980@gmail.com>
2026-08-18 08:25:08 +0000

    code

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ app.get('/',(req, res)=>{
      return `Additon :${5+4}`;
 });
 
-app.listen(3000,()=>{
-  console.log("server started on port 3000");
-});
-
 
 function add(a, b) {
     return a + b;
